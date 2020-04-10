728348432567c70818ce08fdb805a5f5ad5fa769
Mohamed Fazrin <mohamed.fazrin@staff.broadbandsolutions.com.au>
2020-04-10 23:35:06 +0530

    Add project files.

---
+++ Testing/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Testing
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            for (int x = 1; x < 57; x += 5)
+            {
+                if (x == 1)
+                {
+                    Console.WriteLine(x);
+                }
+
+                if (x < 56 && x > 5)
+                {
+                    Console.WriteLine(Convert.ToInt32(x - 1) + ", " + x + ", ");
+                }
+                else if ( x == 56)
+                {
+                    Console.WriteLine(Convert.ToInt32(x -1));
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
